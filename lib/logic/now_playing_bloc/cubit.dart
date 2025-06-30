3e68683a9ee4af521ac9a88e66118f84f1e678cc
AMIT <za.mohamedsaleh011213@gmail.com>
2025-06-30 17:02:49 +0300

    --

--- lib/logic/now_playing_bloc/cubit.dart
+++ lib/logic/now_playing_bloc/cubit.dart
@@ -3,20 +3,25 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:netflixfluttermaadi62/core/api/api_url.dart';
 import 'package:netflixfluttermaadi62/logic/now_playing_bloc/states.dart';
 
-class NowPlayingCubit extends Cubit <NowPlayingStates> {
-  NowPlayingCubit () : super (NowPlayingInitailState());
+import '../../data/list_movie_model.dart';
+
+class NowPlayingCubit extends Cubit<NowPlayingStates> {
+  NowPlayingCubit() : super(NowPlayingInitailState());
 
   final dio = Dio();
-  
-  /// 
-  Future getNowPlayingMovie () async {
+
+  ///
+  Future getNowPlayingMovie() async {
     emit(NowPlayingLoadingState());
-    try{
-      dio.get(ApiUrl.nowPlayingUrl);
-    }catch (e){
+    try {
+      /// GET DATA
+      final response = await dio.get(ApiUrl.nowPlayingUrl);
+      if (response.statusCode == 200) {
+        final x = MovieListResponse.fromJson(response.data);
+        emit(NowPlayingSuccessState(movieListResponse: x));
+      }
+    } catch (e) {
       emit(NowPlayingErrorState(em: e.toString()));
     }
-    
   }
-
 }
--- lib/logic/now_playing_bloc/states.dart
+++ lib/logic/now_playing_bloc/states.dart
@@ -1,10 +1,16 @@
+import '../../data/list_movie_model.dart';
+
 class NowPlayingStates {}
 
 class NowPlayingInitailState extends NowPlayingStates {}
 
 class NowPlayingLoadingState extends NowPlayingStates {}
 
-class NowPlayingSuccessState extends NowPlayingStates {}
+class NowPlayingSuccessState extends NowPlayingStates {
+  final MovieListResponse movieListResponse;
+  NowPlayingSuccessState ({required this.movieListResponse});
+
+}
 
 class NowPlayingErrorState extends NowPlayingStates {
   final String em;
